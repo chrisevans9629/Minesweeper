c4f79d5b1bacaca39b338df49f56894968aaa7aa
Chris Evans <Cevans@peerlessproducts.com>
2019-07-07 10:07:49 -0500

    added the subtract parser

---
+++ Minesweeper.Test/MathParser.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Minesweeper.Test
+{
+    public class MathParser
+    {
+
+        public double Calculate(string value)
+        {
+            var operators = new List<MathValue>();
+            NumberValue previousValue = null;
+            for (var i = 0; i < value.Length; i++)
+            {
+                var currentValue = value[i].ToString();
+                var last = operators.LastOrDefault();
+                if (last is NumberValue v)
+                {
+                    currentValue = v.StringValue + currentValue;
+                    if (double.TryParse(currentValue, out var ts))
+                    {
+                        previousValue = v;
+                        v.Value = ts;
+                        v.StringValue = currentValue;
+                        continue;
+                    }
+                }
+                if (double.TryParse(currentValue, out var t) || currentValue == ".")
+                {
+                    var val = new NumberValue() {Value = t, StringValue = currentValue};
+                    previousValue = val;
+                    
+                    operators.Add(val);
+                }
+                var lastOp = operators.OfType<NumberOperator>().LastOrDefault();
+
+                if (value[i] == '-')
+                {
+                    if (lastOp != null)
+                    {
+                        operators.Add(new SecondSubtract() { First = lastOp });
+                    }
+                    else
+                    {
+                        operators.Add(new Subtract() { First = previousValue });
+
+                    }
+                    continue;
+                }
+
+                if (value[i] == '+')
+                {
+                    if (lastOp != null)
+                    {
+                        operators.Add(new SecondAdd() { First = lastOp });
+                    }
+                    else
+                    {
+                        operators.Add(new Add() { First = previousValue });
+
+                    }
+                    continue;
+                }
+
+                var secAdd = operators.OfType<DoubleOperator>().LastOrDefault();
+                if (secAdd != null && secAdd.Second == null)
+                {
+                    secAdd.Second = previousValue;
+                    continue;
+                }
+                if (lastOp != null && lastOp.Second == null)
+                {
+                    lastOp.Second = previousValue;
+                    continue;
+                }
+            }
+
+            var si = operators.OfType<NumberValue>().FirstOrDefault();
+            if (si != null && operators.Count == 1)
+            {
+                return si.Value ?? 0;
+            }
+           
+            return operators.OfType<Operator>().Sum(p => p.Calculate());
+        }
+    }
+}
--- Minesweeper.Test/ParserTests.cs
+++ Minesweeper.Test/ParserTests.cs
@@ -1,36 +1,53 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using FluentAssertions;
 using NUnit.Framework;
 
 namespace Minesweeper.Test
 {
-    public class SecondAdd : Operator
-    {
-        public Add First { get; set; }
-        public double? Second { get; set; }
-        private bool calculated = false;
-        public override double Calculate()
-        {
-            if (!calculated)
-            {
-                calculated = true;
-                return First.Calculate() + (Second ?? 0);
-            }
 
-            return 0;
+    public class MathValue
+    {
+
+    }
+    public class NumberValue : MathValue
+    {
+        public string StringValue { get; set; }
+        public double? Value { get; set; }
+     
+    }
+
+    public class SecondAdd : DoubleOperator
+    {
+      
+        public override double Calculate(double first, double second)
+        {
+            return first + second;
         }
     }
 
-    public abstract class Operator
+    public class SecondSubtract : DoubleOperator
     {
+
+        public override double Calculate(double first, double second)
+        {
+                return first - second;
+        }
+    }
+    public class Subtract : NumberOperator
+    {
+        public override double Calculate(double first, double second)
+        {
+            return first - second;
+        }
+    }
+    public abstract class Operator : MathValue
+    {
+        public abstract double Calculate(double first, double second);
         public abstract double Calculate();
     }
-    public class Add : Operator
+
+    public abstract class DoubleOperator : Operator
     {
-        public double? First { get; set; }
-        public double? Second { get; set; }
         private bool calculated = false;
 
         public override double Calculate()
@@ -38,58 +55,37 @@ namespace Minesweeper.Test
             if (!calculated)
             {
                 calculated = true;
-                return (First ?? 0) + (Second ?? 0);
+                return Calculate(First.Calculate(),(Second.Value ?? 0));
             }
-
             return 0;
         }
+        public NumberOperator First { get; set; }
+        public NumberValue Second { get; set; }
     }
-    public class MathParser
+    public abstract class NumberOperator : Operator
     {
+        private bool calculated = false;
 
-        public double Calculate(string value)
+        public NumberValue First { get; set; }
+        public NumberValue Second { get; set; }
+        public override double Calculate()
         {
-            var operators = new List<Operator>();
-            var previousValue = 0.0;
-            for (var i = 0; i < value.Length; i++)
+            if (!calculated)
             {
-                var currentValue = value[i].ToString();
-                if (double.TryParse(currentValue, out var t))
-                {
-                    if (value.Length == 1)
-                        return t;
-                    previousValue = t;
-                }
-                var lastOp = operators.OfType<Add>().LastOrDefault();
-
-                if (value[i] == '+')
-                {
-                    if (lastOp != null)
-                    {
-                        operators.Add(new SecondAdd() { First = lastOp });
-                    }
-                    else
-                    {
-                        operators.Add(new Add() { First = previousValue });
-
-                    }
-                    continue;
-                }
-
-                var secAdd = operators.OfType<SecondAdd>().LastOrDefault();
-                if (secAdd != null && secAdd.Second == null)
-                {
-                    secAdd.Second = previousValue;
-                    continue;
-                }
-                if (lastOp != null && lastOp.Second == null)
-                {
-                    lastOp.Second = previousValue;
-                    continue;
-                }
+                calculated = true;
+                return Calculate(First.Value ?? 0, Second.Value ?? 0);
             }
+            return 0;
 
-            return operators.Sum(p => p.Calculate());
+        }
+
+    }
+
+    public class Add : NumberOperator
+    {
+        public override double Calculate(double first, double second)
+        {
+            return first + second;
         }
     }
 
@@ -119,6 +115,15 @@ namespace Minesweeper.Test
         [TestCase("2+2+2", 6)]
         [TestCase("2+2+2+2", 8)]
         [TestCase("2+4", 6)]
+        [TestCase("20+1", 21)]
+        [TestCase("20+15", 35)]
+        [TestCase("20", 20)]
+        [TestCase("200+10", 210)]
+        [TestCase("200-10", 190)]
+        [TestCase("200*10", 2000)]
+        [TestCase("1.5+2.5", 4)]
+        [TestCase(".5+2.5", 3)]
+
         public void Tests(string math, double result)
         {
             parser.Calculate(math).Should().Be(result);
