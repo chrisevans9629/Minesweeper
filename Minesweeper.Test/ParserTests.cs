9e20ab81ede7278f4e4718a1198464e13aa82e6d
Chris Evans <Cevans@peerlessproducts.com>
2019-07-07 00:57:21 -0500

    Added tests

---
+++ Minesweeper.Test/ParserTests.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Minesweeper.Test
+{
+    public class SecondAdd : Operator
+    {
+        public Add First { get; set; }
+        public double? Second { get; set; }
+        private bool calculated = false;
+        public override double Calculate()
+        {
+            if (!calculated)
+            {
+                calculated = true;
+                return First.Calculate() + (Second ?? 0);
+            }
+
+            return 0;
+        }
+    }
+
+    public abstract class Operator
+    {
+        public abstract double Calculate();
+    }
+    public class Add : Operator
+    {
+        public double? First { get; set; }
+        public double? Second { get; set; }
+        private bool calculated = false;
+
+        public override double Calculate()
+        {
+            if (!calculated)
+            {
+                calculated = true;
+                return (First ?? 0) + (Second ?? 0);
+            }
+
+            return 0;
+        }
+    }
+    public class MathParser
+    {
+
+        public double Calculate(string value)
+        {
+            var operators = new List<Operator>();
+            var previousValue = 0.0;
+            for (var i = 0; i < value.Length; i++)
+            {
+                var currentValue = value[i].ToString();
+                if (double.TryParse(currentValue, out var t))
+                {
+                    if (value.Length == 1)
+                        return t;
+                    previousValue = t;
+                }
+                var lastOp = operators.OfType<Add>().LastOrDefault();
+
+                if (value[i] == '+')
+                {
+                    if (lastOp != null)
+                    {
+                        operators.Add(new SecondAdd() { First = lastOp });
+                    }
+                    else
+                    {
+                        operators.Add(new Add() { First = previousValue });
+
+                    }
+                    continue;
+                }
+
+                var secAdd = operators.OfType<SecondAdd>().LastOrDefault();
+                if (secAdd != null && secAdd.Second == null)
+                {
+                    secAdd.Second = previousValue;
+                    continue;
+                }
+                if (lastOp != null && lastOp.Second == null)
+                {
+                    lastOp.Second = previousValue;
+                    continue;
+                }
+            }
+
+            return operators.Sum(p => p.Calculate());
+        }
+    }
+
+    [TestFixture]
+    public class ParserTests
+    {
+        private MathParser parser;
+
+        [SetUp]
+        public void Setup()
+        {
+            parser = new MathParser();
+        }
+
+        [Test]
+        public void Two()
+        {
+            parser.Calculate("2").Should().Be(2);
+        }
+
+        [Test]
+        public void TwoAddTwo()
+        {
+            parser.Calculate("2+2").Should().Be(4);
+        }
+
+        [TestCase("2+2+2", 6)]
+        [TestCase("2+2+2+2", 8)]
+        [TestCase("2+4", 6)]
+        public void Tests(string math, double result)
+        {
+            parser.Calculate(math).Should().Be(result);
+        }
+    }
+}
--- Minesweeper.Test/XorTests.cs
+++ Minesweeper.Test/XorTests.cs
@@ -6,6 +6,63 @@ using NUnit.Framework;
 
 namespace Minesweeper.Test
 {
+
+    public class SingleChar : Component
+    {
+        private readonly char _matchValue;
+
+        public SingleChar(char matchValue)
+        {
+            _matchValue = matchValue;
+        }
+        public override bool AddValueIfValid(char nextValue)
+        {
+            if (_matchValue == nextValue)
+            {
+                this.MatchingCharacters = nextValue.ToString();
+                Value = nextValue;
+                return true;
+            }
+
+            return false;
+        }
+    }
+    public class GoUntil : Component
+    {
+        private readonly Component _from;
+        private readonly Component _to;
+
+        public GoUntil(Component from, Component to)
+        {
+            _from = @from;
+            _to = to;
+        }
+        public override bool AddValueIfValid(char nextValue)
+        {
+            return _from.AddValueIfValid(nextValue) || _to.AddValueIfValid(nextValue);
+        }
+    }
+    public class Number : Component
+    {
+        public override bool AddValueIfValid(char nextValue)
+        {
+            if (double.TryParse(MatchingCharacters + nextValue, out var t))
+            {
+                Value = t;
+                MatchingCharacters += nextValue;
+                return true;
+            }
+
+            return false;
+        }
+    }
+    public abstract class Component
+    {
+        public object Value { get; set; }
+        public string MatchingCharacters { get; set; }
+        public string PreviousCharacters { get; set; }
+        public abstract bool AddValueIfValid(char nextValue);
+    }
     public class PathChosen
     {
         public PathChosen(PathChosen pathChosen)
