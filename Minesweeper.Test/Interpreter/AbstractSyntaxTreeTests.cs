5352d295a76da367191c8ea0c057746aa645e0a7
Chris Evans <chrisevans9629@gmail.com>
2019-07-16 19:35:08 -0500

    tree now handles parenthesese

--- Minesweeper.Test/Interpreter/AbstractSyntaxTreeTests.cs
+++ Minesweeper.Test/Interpreter/AbstractSyntaxTreeTests.cs
@@ -4,6 +4,40 @@ using NUnit.Framework;
 
 namespace Minesweeper.Test
 {
+
+    public class SimpleTreeTests
+    {
+        private SimpleTree tree;
+
+        [SetUp]
+        public void Setup()
+        {
+            tree = new SimpleTree();
+        }
+        [TestCase("14 + 2 * 3 - 6 / 2", 17)]
+        [TestCase("2 * 2", 4)]
+        [TestCase("(1+2)*2", 6)]
+        [TestCase("2 * 2 +2", 6)]
+        [TestCase("7 + 3 * (10 / (12 / (3 + 1) - 1))", 22)]
+
+        public void Evaluate_Test(string input, double output)
+        {
+            var lex = new Lexer();
+            lex.Ignore(" ");
+            lex.Add("LPA", @"\(");
+            lex.Add("RPA", @"\)");
+            lex.Add("NUM", @"\d+");
+            lex.Add("ADD", @"\+");
+            lex.Add("SUB", @"-");
+            lex.Add("MUL", @"\*");
+            lex.Add("DIV", @"/");
+            var tokens = lex.Tokenize(input);
+            var t = tree.Evaluate(tokens);
+
+            t.Should().Be(output);
+        }
+    }
+
     [TestFixture(typeof(SimpleTree))]
     [TestFixture(typeof(AbstractSyntaxTree))]
     public class AbstractSyntaxTreeTests<T> where T : IAbstractSyntaxTree, new()
@@ -25,23 +59,24 @@ namespace Minesweeper.Test
         [TestCase("9 + 5 - 3 + 11", 22)]
         [TestCase("3", 3)]
         [TestCase("7*4/2*3", 42)]
+        [TestCase("14 + 2 * 3 - 6 / 2", 17)]
         public void Evaluate_Test(string input, double output)
         {
             var lex = new Lexer();
             lex.Ignore(" ");
-            lex.Add("Num", @"\d+");
-            lex.Add("Add", @"\+");
-            lex.Add("Sub", @"-");
-            lex.Add("Multi", @"\*");
-            lex.Add("Div", @"/");
+            lex.Add("NUM", @"\d+");
+            lex.Add("ADD", @"\+");
+            lex.Add("SUB", @"-");
+            lex.Add("MUL", @"\*");
+            lex.Add("DIV", @"/");
 
-            tree.AddExpress("Num", (list, d) => Double.Parse(list[0].Value));
+            tree.AddExpress("NUM", (list, d) => Double.Parse(list[0].Value));
             // tree.AddExpress("Num Add Num", (p,_) => (int.Parse(p[0].Value) + int.Parse(p[2].Value)));
-            tree.AddExpress("Add Num", (p, prev) => (prev + double.Parse(p[1].Value)));
-            tree.AddExpress("Sub Num", (p, prev) => (prev - double.Parse(p[1].Value)));
+            tree.AddExpress("ADD NUM", (p, prev) => (prev + double.Parse(p[1].Value)));
+            tree.AddExpress("SUB NUM", (p, prev) => (prev - double.Parse(p[1].Value)));
             // tree.AddExpress("Num Sub Num", (p, _) => (int.Parse(p[0].Value) - int.Parse(p[2].Value)));
-            tree.AddExpress("Multi Num", (p, prev) => (prev * double.Parse(p[1].Value)));
-            tree.AddExpress("Div Num", (p, prev) => (prev / double.Parse(p[1].Value)));
+            tree.AddExpress("MUL NUM", (p, prev) => (prev * double.Parse(p[1].Value)));
+            tree.AddExpress("DIV NUM", (p, prev) => (prev / double.Parse(p[1].Value)));
             var tokens = lex.Tokenize(input);
             var t = tree.Evaluate(tokens);
 
--- Minesweeper.Test/SimpleTree.cs
+++ Minesweeper.Test/SimpleTree.cs
@@ -6,13 +6,16 @@ namespace Minesweeper.Test
 {
     public class SimpleTree : IAbstractSyntaxTree
     {
+        private const string Num = "NUM";
         public void AddExpress(string expression, Func<IList<TokenItem>, double, double> eval)
         {
         }
 
-        double Number(TokenItem token)
+        double Number(IEnumerator<TokenItem> token)
         {
-            return double.Parse(token.Value);
+            var result = double.Parse(token.Current.Value);
+            Eat(token, Num);
+            return result;
         }
         public double Evaluate(IList<TokenItem> tokens)
         {
@@ -20,47 +23,94 @@ namespace Minesweeper.Test
 
             using (var numerator = tokens.GetEnumerator())
             {
-                numerator.MoveNext();
-                var result = Number(numerator.Current);
-                numerator.MoveNext();
+                Eat(numerator, null);
 
-                while (numerator.Current != null && numerator.Current.Token.Name != "Num")
-                {
-                    if (numerator.Current.Token.Name == "Add")
-                    {
-                        numerator.MoveNext();
-                        result += Number(numerator.Current);
-                        numerator.MoveNext();
-                    }
-                    else if(numerator.Current.Token.Name == "Sub")
-                    {
-                        numerator.MoveNext();
-                        result -= Number(numerator.Current);
-                        numerator.MoveNext();
-                    }
-                    else if (numerator.Current.Token.Name == "Multi")
-                    {
-                        numerator.MoveNext();
-                        result *= Number(numerator.Current);
-                        numerator.MoveNext();
-                    }
-                    else if (numerator.Current.Token.Name == "Div")
-                    {
-                        numerator.MoveNext();
-                        result /= Number(numerator.Current);
-                        numerator.MoveNext();
-                    }
-                    else
-                    {
-                        throw new Exception($"Could not find grammer for {numerator.Current}");
-                    }
-                }
+                return Expr(numerator);
+            }
+        }
 
+
+        private const string LParinth = "LPA";
+        private const string RParinth = "RPA";
+        private const string Multi = "MUL";
+        private const string Div = "DIV";
+
+        double Par(IEnumerator<TokenItem> numerator)
+        {
+            if (numerator.Current.Token.Name == LParinth)
+            {
+                Eat(numerator, LParinth);
+                var result = Expr(numerator);
+                Eat(numerator, RParinth);
                 return result;
+            }
+            var num = Number(numerator);
+            return num;
+        }
+        private double Term(IEnumerator<TokenItem> numerator)
+        {
+            //var result = 0.0;
+            //numerator.MoveNext();
+            var result = Par(numerator);
 
+            while (numerator.Current != null && numerator.Current.Token.Name != Num)
+            {
+                if (numerator.Current.Token.Name == Multi)
+                {
+                    Eat(numerator, Multi);
+                    result *= Par(numerator);
+                    //numerator.MoveNext();
+                }
+                else if (numerator.Current.Token.Name == Div)
+                {
+                    Eat(numerator, Div);
+                    result /= Par(numerator);
+                    //numerator.MoveNext();
+                }
+                else
+                {
+                    break;
+                }
             }
 
+            return result;
+        }
 
+        void Eat(IEnumerator<TokenItem> num, string item)
+        {
+            var name = num.Current?.Token?.Name;
+            if (name != item) throw new Exception($"tried to eat '{item}' but was {name}");
+            num.MoveNext();
+        }
+
+        private const string Add = "ADD";
+        private const string Sub = "SUB";
+        private double Expr(IEnumerator<TokenItem> numerator)
+        {
+
+            var result = Term(numerator);
+
+            while (numerator.Current != null && numerator.Current.Token.Name != Num)
+            {
+                if (numerator.Current.Token.Name == Add)
+                {
+                    Eat(numerator, Add);
+                    result += Term(numerator);
+                    //numerator.MoveNext();
+                }
+                else if (numerator.Current.Token.Name == Sub)
+                {
+                    Eat(numerator, Sub);
+                    result -= Term(numerator);
+                    //numerator.MoveNext();
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return result;
         }
     }
 }
