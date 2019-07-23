9893c1899315205684ed88296ed774bfd4c4a12b
Chris Evans <Cevans@peerlessproducts.com>
2019-07-23 16:21:17 -0500

    added symbol builder and moved tests

---
+++ Minesweeper.Test/Interpreter/Symbols/SymbolTableBuilder.cs
@@ -0,0 +1,18 @@
+﻿namespace Minesweeper.Test.Symbols
+{
+    public class SymbolTableBuilder
+    {
+        SymbolTable table = new SymbolTable();
+
+        public SymbolTable CreateTable(Node rootNode)
+        {
+            VisitNode(rootNode);
+            return table;
+        }
+
+        private void VisitNode(Node rootNode)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
