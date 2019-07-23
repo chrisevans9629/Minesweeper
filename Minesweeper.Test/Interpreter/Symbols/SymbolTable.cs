5566c2cead688d025a2809c46ff7a576295179de
Chris Evans <chrisevans9629@gmail.com>
2019-07-22 20:51:54 -0500

    symbol table works

---
+++ Minesweeper.Test/Interpreter/Symbols/SymbolTable.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Minesweeper.Test.Symbols
+{
+    public class SymbolTable
+    {
+        IList<Symbol> symbols = new List<Symbol>();
+
+        public SymbolTable()
+        {
+            Define(new BuiltInTypeSymbol(Pascal.Int));
+            Define(new BuiltInTypeSymbol(Pascal.Real));
+        }
+        public void Define(Symbol symbol)
+        {
+            Console.WriteLine($"Define: {symbol}");
+            symbols.Add(symbol);
+        }
+
+        public override string ToString()
+        {
+            var str = "Symbols: ";
+            foreach (var symbol in symbols)
+            {
+                str += symbol.Name + ", ";
+            }
+
+            return str;
+        }
+
+        public Symbol LookupSymbol(string name)
+        {
+            Console.WriteLine($"Lookup: {name}");
+            return symbols.FirstOrDefault(p => p.Name == name);
+        }
+    }
+}
