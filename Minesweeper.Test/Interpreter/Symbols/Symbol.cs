14208134bcea631e2f9e66d5eb600fb53fa0b7e6
Chris Evans <chrisevans9629@gmail.com>
2019-07-22 20:43:42 -0500

    added symbols

---
+++ Minesweeper.Test/Interpreter/Symbols/Symbol.cs
@@ -0,0 +1,38 @@
+﻿namespace Minesweeper.Test.Symbols
+{
+    public class Symbol
+    {
+        public string Name { get; }
+        public string Type { get; }
+
+        public Symbol(string name, string type = null)
+        {
+            Name = name;
+            Type = type;
+        }
+    }
+
+    public class BuiltInTypeSymbol : Symbol
+    {
+        public BuiltInTypeSymbol(string name) : base(name)
+        {
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+
+    public class VariableSymbol : Symbol
+    {
+        public VariableSymbol(string name, string type = null) : base(name, type)
+        {
+        }
+
+        public override string ToString()
+        {
+            return $"<{Name}:{Type}>";
+        }
+    }
+}
