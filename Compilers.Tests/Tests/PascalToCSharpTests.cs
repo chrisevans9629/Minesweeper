b1bac629e2115941e8a523ca761cab6297a5038f
Chris Evans <chrisevans9629@gmail.com>
2019-07-30 21:15:50 -0500

    adding pascal to csharp

---
+++ Compilers.Tests/Tests/PascalToCSharpTests.cs
@@ -0,0 +1,32 @@
+﻿using FluentAssertions;
+using Minesweeper.Test.Symbols;
+using NUnit.Framework;
+
+namespace Minesweeper.Test.Tests
+{
+    [TestFixture]
+    public class PascalToCSharpTests
+    {
+        private PascalLexer lexer;
+        private PascalAst ast;
+        private PascalSemanticAnalyzer table;
+        PascalToCSharp cSharp;
+        [SetUp]
+        public void Setup()
+        {
+            lexer = new PascalLexer();
+            ast = new PascalAst();
+            table = new PascalSemanticAnalyzer();
+            cSharp = new PascalToCSharp();
+        }
+
+        [TestCase("program test; var asdf : integer;begin end.", "public static class test\n{\nstatic int asdf;\npublic static void Main()\n{\n}\n}")]
+        public void PascalInput_Should_CreateOutput(string input, string output)
+        {
+            var tokens = lexer.Tokenize(input);
+            var node = ast.Evaluate(tokens);
+            table.CheckSyntax(node);
+            cSharp.VisitNode(node).Should().Be(output);
+        }
+    }
+}
---
+++ Compilers/Interpreter/PascalToCSharp.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Minesweeper.Test
+{
+    public class PascalToCSharp
+    {
+        public string VisitNode(Node node)
+        {
+            if (node is PascalProgramNode program)
+            {
+                return VisitProgram(program).Trim();
+            }
+
+            if (node is VarDeclarationNode varDeclaration)
+            {
+                return VisitVariableDeclaration(varDeclaration);
+            }
+
+            if (node is NoOp)
+            {
+                return "";
+            }
+            throw new NotImplementedException($"Not done: {node}");
+        }
+
+        private string VisitVariableDeclaration(VarDeclarationNode varDeclaration)
+        {
+            var typeValue = varDeclaration.TypeNode.TypeValue.ToUpper();
+            if (Pascal.Int == typeValue)
+            {
+                typeValue = "int";
+            }
+
+            if (Pascal.Real == typeValue)
+            {
+                typeValue = "double";
+            }
+
+            if (Pascal.Boolean == typeValue)
+            {
+                typeValue = "bool";
+            }
+            return $"static {typeValue} {varDeclaration.VarNode.VariableName};\n";
+        }
+
+        private string VisitProgram(PascalProgramNode program)
+        {
+            return $"public static class {program.ProgramName}\n{VisitBlock(program.Block, "Main", "void")}\n";
+        }
+
+        private string VisitBlock(BlockNode block, string name, string type)
+        {
+            return "{\n" + $"{VisitNodes(block.Declarations)}{VisitCompoundStatment(block.CompoundStatement, name, type)}" + "}\n";
+        }
+
+        private string VisitCompoundStatment(CompoundStatement compoundStatement, string name, string type)
+        {
+            return $"public static {type} {name}()\n"+"{\n" + VisitNodes(compoundStatement.Nodes) + "}\n";
+        }
+
+        private string VisitNodes(IList<Node> blockDeclarations)
+        {
+            var str = "";
+            foreach (var blockDeclaration in blockDeclarations)
+            {
+                str += VisitNode(blockDeclaration);
+            }
+            return str;
+        }
+    }
+}
