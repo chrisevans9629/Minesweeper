b4f269d854f15c52dcbc445dccd5ac1dcc535e08
Chris Evans <chrisevans9629@gmail.com>
2019-08-08 20:25:56 -0500

    build targets work!

---
+++ Compilers/Build/BuildPascal.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Build.Utilities;
+
+namespace Compilers.Build
+{
+    using Microsoft.Build.Utilities;
+
+    public class BuildPascal : Task
+    {
+        public override bool Execute()
+        {
+            Log.LogError("hello world!");
+            return true;
+        }
+    }
+}
