6bc4ec747b83e70de0a17fc405acf566350e4c9f
Chris Evans <chrisevans9629@gmail.com>
2019-08-18 21:01:49 -0500

    added il project

---
+++ ILTestProject/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace ILTestProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Square(10));
+            Console.ReadLine();
+        }
+
+        [MethodImpl(MethodImplOptions.ForwardRef)]
+        public static extern int Square(int number);
+        [MethodImpl(MethodImplOptions.ForwardRef)]
+        public static extern int Factorial(int x);
+    }
+}
