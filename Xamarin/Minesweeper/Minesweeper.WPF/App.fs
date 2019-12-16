7ba25bcdbcec0045607edd399389a7c5e5ec8007
Chris Evans <Cevans@peerlessproducts.com>
2019-12-15 22:26:16 -0600

    added fabulous

---
+++ Xamarin/Minesweeper/Minesweeper.WPF/App.fs
@@ -0,0 +1,21 @@
+﻿namespace Minesweeper.WPF
+
+open System
+
+open Xamarin.Forms
+open Xamarin.Forms.Platform.WPF
+
+type MainWindow() = 
+    inherit FormsApplicationPage()
+
+module Main = 
+    [<EntryPoint>]
+    [<STAThread>]
+    let main(_args) =
+
+        let app = new System.Windows.Application()
+        Forms.Init()
+        let window = MainWindow() 
+        window.LoadApplication(new Minesweeper.App())
+
+        app.Run(window)
